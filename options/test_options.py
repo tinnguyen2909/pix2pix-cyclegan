937bdcd8fef1b7526ddb86b27063634b5472a727
Tin <tin@olli-ai.com>
2024-11-06 08:36:14 +0700

    update test options

--- options/test_options.py
+++ options/test_options.py
@@ -9,12 +9,23 @@ class TestOptions(BaseOptions):
 
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)  # define shared options
-        parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
-        parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
-        parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
+        parser.add_argument('--results_dir', type=str,
+                            default='./results/', help='saves results here.')
+        parser.add_argument('--aspect_ratio', type=float,
+                            default=1.0, help='aspect ratio of result images')
+        parser.add_argument('--phase', type=str,
+                            default='test', help='train, val, test, etc')
         # Dropout and Batchnorm has different behavioir during training and test.
-        parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
-        parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
+        parser.add_argument('--eval', action='store_true',
+                            help='use eval mode during test time.')
+        parser.add_argument('--num_test', type=int, default=50,
+                            help='how many test images to run')
+        parser.add_argument('--path_A', type=str, help='path to A', default="")
+        parser.add_argument('--path_B', type=str, help='path to B', default="")
+        parser.add_argument('--ext_A', type=str, default='jpg',
+                            help='extension of A (eg: jpg, png)')
+        parser.add_argument('--ext_B', type=str, default='png',
+                            help='extension of B (eg: jpg, png)')
         # rewrite devalue values
         parser.set_defaults(model='test')
         # To avoid cropping, the load_size should be the same as crop_size
